1e08a8027d9c379fd3245a859b1181c68b9fb6c5
Todd Reifsteck <toddreif@microsoft.com>
2016-05-16 15:53:52 -0700

    Add a time-out of 30 seconds for navigates (avoids perma-hang when a particulate site is having issues)

--- EduPerfTests/PageLoad.cs
+++ EduPerfTests/PageLoad.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using static EduPerfTests.Utils;
 
@@ -10,12 +11,14 @@ namespace EduPerfTests
     {
         private PerformanceLog _perfLog;
         private string _scheme;
+        private Stopwatch _watch;
 
         public PageLoad(string scheme)
         {
             _perfLog = new PerformanceLog("pageloadtestresults");
             _perfLog.InitializeLog("Site,Browser,Result (ms),Iteration,Error");
             _scheme = scheme;
+            _watch = Stopwatch.StartNew();
         }
 
         public void LoadPages(List<string> pageLoadSites, Browser chosenBrowsers, int iterations)
@@ -33,35 +36,56 @@ namespace EduPerfTests
         }
 
         private void SiteLoadTime(string site, Browser browser, RemoteWebDriver driver, int iterations)
-        {
+        {            
             string fullUrl = $"{_scheme}://{site}";
             for (int i = 0; i < iterations; i++)
             {
                 string error = string.Empty;
-                Console.WriteLine($"Recording Site Load Time For '{fullUrl}'");
-
-                // driver.Url = site seems to fail in Chrome 50 and later. If scheme is added, navigations work.
-                driver.Navigate().GoToUrl(fullUrl);
-
-                if (browser == Browser.InternetExplorer)
+                long result = -1;
+                try
                 {
-                    Thread.Sleep(500);
-                }
+                    Console.WriteLine($"Recording Site Load Time For '{fullUrl}'");
 
-                long loadEventEnd;
-                while (true)
+                    // driver.Url = site seems to fail in Chrome 50 and later. If scheme is added, navigations work.
+                    driver.Navigate().GoToUrl(fullUrl);
+
+                    if (browser == Browser.InternetExplorer)
+                    {
+                        Thread.Sleep(500);
+                    }
+
+                    long loadEventEnd;
+                    long navStarted = _watch.ElapsedMilliseconds;
+                    while (true)
+                    {
+                        // This loop is required because Microsoft Edge and Firefox both sometimes return from .Url earlier than they should
+                        var loadEventObject = driver.ExecuteScript("return performance.timing.loadEventEnd;");
+                        loadEventEnd = Convert.ToInt64(loadEventObject);
+                        if (loadEventEnd != 0) break;
+                        error = "driver.Navigate().GoToUrl(fullUrl); is returning early";
+                        if (_watch.ElapsedMilliseconds - navStarted > 30000)
+                        {
+                            throw new TimeoutException("Navigate took > 30 seconds.");
+                        }
+                    }
+
+                    var navStartObject = driver.ExecuteScript("return performance.timing.navigationStart;");
+                    long navStart = Convert.ToInt64(navStartObject);
+                    result = loadEventEnd - navStart;
+                }
+                catch (TimeoutException e)
                 {
-                    // This loop is required because Microsoft Edge and Firefox both sometimes return from .Url earlier than they should
-                    var loadEventObject = driver.ExecuteScript("return performance.timing.loadEventEnd;");
-                    loadEventEnd = Convert.ToInt64(loadEventObject);
-                    if (loadEventEnd != 0) break;
-                    error = "driver.Navigate().GoToUrl(fullUrl); is returning early";
+                    error = e.Message;
                 }
-
-                var navStartObject = driver.ExecuteScript("return performance.timing.navigationStart;");
-                long navStart = Convert.ToInt64(navStartObject);
-                var result = loadEventEnd - navStart;
-                _perfLog.WriteToLog($"{site},{browser},{result},{i + 1},");
+                catch (Exception e)
+                {
+                    error = e.Message;
+                    throw;
+                }
+                finally
+                {
+                    _perfLog.WriteToLog($"{fullUrl},{browser},{result},{i + 1},{error}");
+                }
             }
         }
     }
